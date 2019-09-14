ced0cfba91f398cf6ee03937265a558db9dcb2a5
Katie Lewis <klewi314@gmail.com>
2019-09-14 14:12:25 -0700

    added check score functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,19 +29,22 @@ let characters = [{
   }
 ]
 //Populates when user selects a character
-// let attacker;
+let attackChar;
 
 //Holds characters that user did not select
 let leftovers = [];
 
 //Populated when player chooses an opponent
-// let opponent;
+let oppChar;
 
 let isAttacker = false
 let isOpponent = false
+isAlive = true
+
+let attackPoints = 0
 //Keep track of turns during combat - used for calcuating player damage
 
-//Tracks number of defeated opponents
+//random number for attack 
 
 //Functions
 
@@ -72,16 +75,16 @@ charactersDiv.append(cardDiv)
  
 };
 
-renderCharacter()
+
 
 //onclick function to trigger opponent/attacker
 document.addEventListener('click', ({
   target: e
 }) =>{
   if (!isAttacker){
-    selectCharacter(characters[e.id])
+    let attackChar = selectCharacter(characters[e.id])
   } else if (!isOpponent){ 
-    selectOpp(characters[e.id])
+   let oppChar = selectOpp(characters[e.id])
   }
 })
 
@@ -94,7 +97,7 @@ document.addEventListener('click', ({
 let selectCharacter=(char)=>{
   // let attacker = char
   isAttacker = true
-  let target = char.name
+  attackChar = characters[char.id]
   console.log(`your characters is ${char.name}`)
   document.querySelector(`.${char.name}`).className = "hide"
   let attackerDiv = document.createElement('div')
@@ -105,17 +108,16 @@ let selectCharacter=(char)=>{
       </div>
       <div class="card-content">
       <h4 class="card-title">${char.name}</h4>
-        <p>${char.info}</p>
       </div>
       <div class="card-action">
-        <p> HP: ${char.hp} </p>
-        <button id = ${char.id}> Choose me! </button>
+        <p id = "attackerHp"> HP: ${char.hp} </p>
       </div>
     </div>`
     attackerDiv.className = "col sm12 m3"
 let attacker = document.querySelector(".attacker")
 attacker.append(attackerDiv)
 //Set other card as display none
+renderMessage()
 
 }
 
@@ -124,6 +126,7 @@ attacker.append(attackerDiv)
 let selectOpp = (char) =>{
   if (isAttacker){
   isOpponent = true
+  oppChar = characters[char.id]
   document.querySelector(`.${char.name}`).className = "hide"
   let oppDiv = document.createElement('div')
   oppDiv.innerHTML = ` 
@@ -133,11 +136,9 @@ let selectOpp = (char) =>{
       </div>
       <div class="card-content">
       <h4 class="card-title">${char.name}</h4>
-        <p>${char.info}</p>
       </div>
       <div class="card-action">
-        <p> HP: ${char.hp} </p>
-        <button id = ${char.id}> Choose me! </button>
+        <p id = "oppHp"> HP: ${char.hp} </p>
       </div>
     </div>`
     oppDiv.className = "col sm12 m3"
@@ -147,21 +148,65 @@ let battleBtn = document.createElement('button')
 battleBtn.className = "waves-effect waves-light btn-small"
 battleBtn.innerText = "BATTLE"
 document.querySelector('.button').append(battleBtn)
+renderMessage()
 }
 }
 
 //Function to handle rendering game messages
 let renderMessage = () =>{
+  if(!isAttacker && !isOpponent){
+    document.querySelector('#message').innerHTML = "Please choose your character to start the battle!"
+  } else if(isAttacker && !isOpponent){
+    document.querySelector('#message').innerHTML = "Please choose your attacker!!!"
+  } else if (isAttacker && isOpponent){
+    document.querySelector('#message').innerHTML = "Let the battle begin!!! "
+  }
+}
 
-  //builds message and appends it to the page
+
+document.querySelector('.button').addEventListener("click", e=>{
+console.log("butttttttttttons")
+//run attack function
+attack(attackChar, oppChar)
+})
+
+let attack = (attacker, opponent)=>{
+generateNum()
+let attackerHp = attacker.hp -= attackPoints
+generateNum()
+let opponentHp = opponent.hp -= attackPoints
+console.log(`here are the numbers you are passing in: ${attackerHp} & ${opponentHp}`)
+checkHealth(attackerHp, opponentHp)
+}
+
+//Check health
+let checkHealth=(attackerHp,opponentHp)=>{
+  console.log(`The health of attacker: ${attackerHp} Health of opp: ${opponentHp} `)
+if (attackerHp>0 && opponentHp>0){
+  console.log("keep battling")
+  document.querySelector('#oppHp').textContent = "HP: " + opponentHp
+  document.querySelector('#attackerHp').textContent = "HP: " + attackerHp
+}
+else if(attackerHp<0){
+  console.log("you lost")
+  document.querySelector('#oppHp').textContent = "HP: " + opponentHp
+  document.querySelector('#attackerHp').textContent = "HP: 0"
+} 
+else if(opponentHp<0){
+  console.log("you won")
+  document.querySelector('#oppHp').textContent = "HP: 0" 
+  document.querySelector('#attackerHp').textContent = "HP: " + attackerHp
+}
 }
 
 //Function which handles restarting the game ater victory or defeat
-let resetGame = (resultMessage) =>{
+let resetGame = () =>{
   //When restart button is clicked, reload the page
 document.querySelector('.attacker').innerHtml = ""
 document.querySelector('.opponent').innerHTML = ""
-document.querySelectoer('.row').innerHTML = ""
+document.querySelector('.row').innerHTML = ""
+isAttacker = false
+isOpponent = false
 renderCharacter()
 }
 
@@ -173,3 +218,14 @@ renderCharacter()
 
 //function to clear the game message section
 
+generateNum = ()=>{
+  attackPoints =  Math.floor(Math.random() * 30) + 20 
+  return attackPoints
+}
+
+let startGame = ()=>{
+  renderMessage()
+  renderCharacter()
+}
+
+startGame()
